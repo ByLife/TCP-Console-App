0c679890a0897078fe3de336a877733c75cc4cfc
ByLife <bylife@bylife.fr>
2023-03-15 00:02:44 +0100

    v0.1.1

---
+++ src/console/login/index.js
@@ -0,0 +1,6 @@
+const chalk = require("chalk")
+
+module.exports = (client, socket, data) => {
+    console.log(chalk.red("Data: " + data.toString()))
+    console.log("Execute")
+}
--- src/server/index.js
+++ src/server/index.js
@@ -1,24 +1,30 @@
 const net = require('net');
 const chalk = require('chalk');
 const {dirname} = require('path');
-const config = require("./../../config.json")
+const config = require("./../../config.json");
+const Logger = require('../logger');
+const fs = require('fs');
 
 module.exports = new class Server extends net.Server {
   constructor() {
     super();
-    console.log(dirname(require.main.filename) + "\\src\\events");
-    this.on('connection', socket => {
-      socket.write(require("./../console/animations/welcome")(socket))
-      require('fs').readdirSync(dirname(require.main.filename) + "\\src\\events").forEach(file => {
-        const event = file.split('.')[0];
-        socket.on(event, require(`./../events/${file}`).bind(null, this, socket));
+    try {
+      console.log(dirname(require.main.filename) + "\\src\\events");
+      this.on('connection', async socket => {
+        socket.write(require("./../console/animations/welcome")(socket))
+        fs.readdirSync(dirname(require.main.filename) + "\\src\\events").forEach(file => {
+          const event = file.split('.')[0];
+          socket.on(event, require(`./../events/${file}`).bind(null, this, socket));
+        });
+        if(config.application.login.enabled) await socket.on("login", require("./../console/login").bind(null, this, socket))
+        
+      });    
+      this.listen(3000, () => {
+          console.log(chalk.green('Server is listening on port 3000'));
       });
-      if(config.application.login.enabled) require("./../console/login").bind(null, this, socket)
-  
-    });    
-    this.listen(3000, () => {
-        console.log(chalk.green('Server is listening on port 3000'));
-    });
+    } catch (error) {
+      console.log(Logger.error(error))
+    }
   }
 
 }
